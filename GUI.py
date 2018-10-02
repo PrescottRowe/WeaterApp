3c28fff32f35686418b028a433b0ccf10d8867e7
Prescott Rowe <prescottrowe@gmail.com>
2018-10-01 19:32:41 -0700

    working framework
    has search page, then a main page that shows weather in text only format

--- GUI.py
+++ GUI.py
@@ -1,59 +1,75 @@
-import kivy
-kivy.require('1.1.3')
-
-from kivy.properties import NumericProperty
+from googleAPI import GoogleAPICaller
+from darkskyAPI import DarkSkyAPICaller
+from forecast import Forecast
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.anchorlayout import AnchorLayout
-from kivy.uix.button import Button
-from kivy.uix.widget import Widget
-from kivy.uix.textinput import TextInput
-from kivy.uix.scatter import Scatter
-from kivy.uix.treeview import TreeView, TreeViewLabel
-from kivy.uix.label import Label
-from kivy.uix.popup import Popup
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.lang import Builder
 from kivy.properties import StringProperty, ObjectProperty
-from kivy.clock import Clock
-import random
 
 
 class SearchScreen(Screen):
-    def __init__(self, **kwargs):
-        super(SearchScreen, self).__init__(**kwargs)
+    location = StringProperty('No location')
 
+    def set_location(self, address):
+        self.location = str(address)
+        self.manager.current = "ForecastScreen"
 
-    def debug(self, val):
-        if val:
-            try:
-                enterd = str(val)
-                popup = Popup(title='Test popup',
-                              content=Label(text=str(enterd)),
-                              size_hint=(None, None), size=(150, 150))
-                popup.open()
-            except Exception:
-                entered="error"
-                popup = Popup(title='Test popup',
-                              content=Label(text=str(entered)),
-                              size_hint=(None, None), size=(100, 100))
-                popup.open()
 
 class ForecastScreen(Screen):
-    def __init__(self, **kwargs):
-        super(ForecastScreen, self).__init__(**kwargs)
-    def getForecast(self,location):
-        self.location=location
+    label_location = StringProperty('')
+    address = StringProperty('')
+    cTime = StringProperty('')
+    cTemp = StringProperty('')
+    cIcon = StringProperty('')
+    cSummary = StringProperty('')
+    cHumidity = StringProperty('')
+    cUVIndex = StringProperty('')
+    cOzone = StringProperty('')
+    cWindSpeed = StringProperty('')
+    cWindGust = StringProperty('')
+    cVisibility = StringProperty('')
 
-class ScreenManagement(ScreenManager):
-    pass
+    def get_data(self):
+        isAddressLegit=False
+        while isAddressLegit == False:
+            googleAPICaller.setJsonData(self.label_location)
+            isAddressLegit = googleAPICaller.checkIfValidAddress()
+            if isAddressLegit == True:
+                predictionList = googleAPICaller.predictLocation(self.label_location)
+                address = predictionList[0]
+                googleAPICaller.setJsonData(address)
+                googleAPICaller.setAddress()
 
-pressentation = Builder.load_file("Weather.kv")
+        latitude = googleAPICaller.getLatitude()
+        longitude = googleAPICaller.getLongitude()
+        forecastData = darkSkyAPICaller.getForecastJsonData(latitude, longitude)
+        formattedAddress = googleAPICaller.getFormattedAddress()
+        self.fcast = Forecast(forecastData, formattedAddress)
+
+        self.address = str(self.fcast.address)
+        self.cTime = str(self.fcast.cTime)
+        self.cTemp = str(self.fcast.cTemp)
+        self.cIcon = str(self.fcast.cIcon)
+        self.cSummary = str(self.fcast.cSummary)
+        self.cHumidity = str(self.fcast.cHumidity)
+        self.cUVIndex= str(self.fcast.cUVIndex)
+        self.cOzone = str(self.fcast.cOzone)
+        self.cWindSpeed = str(self.fcast.cWindSpeed)
+        self.cWindGust = str(self.fcast.cWindGust)
+        self.cVisibility = str(self.fcast.cVisi)
+
+
+googleKey = 'AIzaSyAO-zV0XBi8hM0pnAtLDdoSVtmhWrl8JWM'
+darkSkyKey = '4a05668b390431d4f52a20934a3f67ac'
+googleAPICaller = GoogleAPICaller(googleKey)
+darkSkyAPICaller = DarkSkyAPICaller(darkSkyKey)
+sm=Builder.load_file('weather.kv')
 class WeatherApp(App):#app class must match kv file minus "app"
     def build(self):#pass the instance of the class to the function so it knows it parent.
-        return pressentation
-
+        return sm
 
 if __name__ == "__main__":#only autorun the code if the file is being run as main.
     WeatherApp().run()
+
+
+
